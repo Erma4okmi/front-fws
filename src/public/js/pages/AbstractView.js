28c4d18b3f253449d0300b6ab841c59b6aec760d
Ermak <57521545+Erma4okmi@users.noreply.github.com>
2022-06-04 18:50:16 +0300

    Update AbstractView.js

--- src/public/js/pages/AbstractView.js
+++ src/public/js/pages/AbstractView.js
@@ -1,13 +1,24 @@
+// Дефолтный класс.
 export default class {
+
+    // Конструктор.
     constructor(params) {
+
+        // Получаем параметры.
         this.params = params;
     }
 
+    // Меняем название страницы.
     setTitle(title) {
+
+        // Меняем title.
         document.title = title;
     }
 
+    // Получаем html.
     async getHtml() {
+
+        // Вернуть.
         return "";
     }
 }
