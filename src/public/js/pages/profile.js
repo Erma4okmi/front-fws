128a5ab5c96f6dbff89b2e84ed04a1acd75b800f
Ermak <57521545+Erma4okmi@users.noreply.github.com>
2022-06-04 18:54:49 +0300

    Update profile.js

--- src/public/js/pages/profile.js
+++ src/public/js/pages/profile.js
@@ -1,12 +1,23 @@
+// Подключаем основу.
 import AbstractView from "./AbstractView.js";
 
+// Стандартный класс унаследован от основы.
 export default class extends AbstractView {
+
+    // Конструктор с параметрами.
     constructor(params) {
+
+        // Кидаем в рожительсикй класс.
         super(params);
+
+        // Получаем параметры.
         this.postId = params.id;
+
+        // Меняем Title.
         this.setTitle("Профиль");
     }
 
+    // Выводим html.
     async getHtml() {
         return `
             <h1>Профиль</h1>
