dfc6abb2b4f64a2f8a0ded2f9fabd1b1ab158dc4
Ermak <57521545+Erma4okmi@users.noreply.github.com>
2022-06-04 18:55:05 +0300

    Update settings.js

--- src/public/js/pages/settings.js
+++ src/public/js/pages/settings.js
@@ -1,11 +1,20 @@
+// Подключаем основу.
 import AbstractView from "./AbstractView.js";
 
+// Стандартный класс унаследован от основы.
 export default class extends AbstractView {
+
+    // Конструктор с параметрами.
     constructor(params) {
+
+        // Кидаем в рожительсикй класс.
         super(params);
+
+        // Меняем Title.
         this.setTitle("Settings");
     }
-
+    
+    // Выводим html.
     async getHtml() {
         return `
             <h1>Настройки</h1>
