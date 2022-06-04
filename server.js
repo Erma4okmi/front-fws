efc0d0ccd821c7bed2d213ddfd13f2524ae081e3
Ermak <57521545+Erma4okmi@users.noreply.github.com>
2022-06-04 18:37:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,4 +9,11 @@ app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "src", "index.html"));
 });
 
-app.listen(process.env.PORT || 8080, () => console.log("Server running... HOST: 172.20.10.10:8080"));
+app.listen(process.env.PORT || 8080, () => {
+
+    // Вывод информации.
+    console.log("Server started successfully! \n")
+    console.log("HOST: localhost:8080 \n")
+    console.log("Ctrl + C - it`s stop server\n")
+
+});
